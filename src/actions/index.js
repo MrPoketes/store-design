19ce4a7d53aff2450fc9da3ed682b24f6591fa66
MrPoketes <az778@york.ac.uk>
2020-08-13 23:47:13 +0300

    changed env variable

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const url = process.env.URL || "http://localhost:8081";
+const url = process.env.REACT_APP_URL || "http://localhost:8081";
 console.log(process.env.URL);
 
 // Product actions
